c620c780ebcbdd208ade09aec40e366472c48ed3
strazan <sigge@labor.cool>
2019-10-04 06:39:27 +0200

    working pulsing grey nebula based on travis ci data

---
+++ app.js
@@ -0,0 +1,112 @@
+let ws = null;
+
+let cloudParticles = [];
+let loader = new THREE.TextureLoader();
+let nebula = nebulaNew();
+let ambient = new THREE.AmbientLight(0x555555);
+
+let scene = new THREE.Scene();
+scene.fog = new THREE.FogExp2(0xaaaaaa, 0.001);
+scene.add(ambient);
+scene.add(nebula);
+
+let camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.z = 1;
+camera.rotation.x = 1.16;
+camera.rotation.y = -0.12;
+camera.rotation.z = 0.27;
+
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(scene.fog.color);
+document.body.appendChild(renderer.domElement);
+
+let buffer = 0;
+
+let onmessage = function (e) {
+
+    if (e.data[0] != '{') return;
+    let data = JSON.parse(e.data)
+
+    buffer++;
+};
+
+function loop() {
+    update();
+    render();
+    requestAnimationFrame(loop);
+}
+
+function update() {
+    if (nebula.scale.x > 0.3) {
+        nebula.scale.x -= 0.001;
+        nebula.scale.z -= 0.001;
+    }
+}
+
+function render() {
+    cloudParticles.forEach(p => {
+        p.rotation.z -= 0.0022;
+    });
+    renderer.render(scene, camera);
+}
+
+function nebulaGrow() {
+
+    let grow = setInterval(function () {
+        if (nebula.scale.x < 2) {
+            nebula.scale.x += 0.002;
+            nebula.scale.z += 0.002;
+        }
+
+        if (nebula.scale.x >= 0.8 + (buffer / 500)) {
+            clearInterval(grow);
+            buffer = 0;
+        }
+    }, 2);
+
+    scene.add(nebula);
+}
+
+function nebulaNew() {
+    let nebula = new THREE.Object3D();
+    loader.load("images/nebula/smoke-1.png", function (texture) {
+        let cloudGeo = new THREE.PlaneBufferGeometry(500, 500);
+        let cloudMaterial = new THREE.MeshLambertMaterial({
+            map: texture,
+            transparent: true
+        });
+
+        for (let p = 0; p < 54; p++) {
+            let cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
+            cloud.position.set(
+                Math.random() * 800 - 400,
+                500,
+                Math.random() * 500 - 500
+            );
+            cloud.rotation.x = 1.16;
+            cloud.rotation.y = -0.12;
+            cloud.rotation.z = Math.random() * 2 * Math.PI;
+            cloud.material.opacity = 0.55;
+            cloudParticles.push(cloud);
+            nebula.add(cloud);
+        }
+    });
+    return nebula;
+}
+
+function startWS() {
+    ws = new WebSocket('wss://travis.durieux.me/');
+    if (onmessage != null) {
+        ws.onmessage = onmessage;
+    }
+    ws.onclose = function () {
+        setTimeout(function () {
+            startWS()
+        }, 5000);
+    };
+}
+
+loop();
+setInterval(nebulaGrow, 2000);
+startWS();
